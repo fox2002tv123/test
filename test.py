b9c639aa4abdde3e209ace67fc13adf8bf913b3a
fox2002tv123 <85301925@qq.com>
2022-04-10 17:08:24 +0800

    第一次提交

---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+Author: bob
+Date: 2022-04-10 16:55:44
+LastEditors: bob
+LastEditTime: 2022-04-10 16:55:46
+FilePath: \test\test.py
+Description: test
+
+Copyright (c) 2022 by bob, All Rights Reserved. 
+'''
+print('git,ok')
---
+++ test2.py
@@ -0,0 +1,11 @@
+'''
+Author: bob
+Date: 2022-04-10 17:02:31
+LastEditors: bob
+LastEditTime: 2022-04-10 17:02:31
+FilePath: \test\test2.py
+Description: aa
+
+Copyright (c) 2022 by bob, All Rights Reserved. 
+'''
+print('test2')
